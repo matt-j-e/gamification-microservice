7f8a7f0007853c70a67808afc1a6cdb750db2876
mattedwards <the.matt.edwards@googlemail.com>
2022-02-17 05:17:17 +0000

    Domain model complete. Entities built.

---
+++ src/main/java/microservices/book/gamification/game/domain/BadgeType.java
@@ -0,0 +1,23 @@
+package microservices.book.gamification.game.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Enumeration with the different types of Badge that can be won.
+ */
+@RequiredArgsConstructor
+@Getter
+public enum BadgeType {
+
+  // Badges depending on score
+  BRONZE("Bronze"),
+  SILVER("Silver"),
+  GOLD("Gold"),
+
+  // Other badges won for different conditions
+  FIRST_WON("First time"),
+  LUCKY_NUMBER("Lucky number");
+
+  private final String description;
+}
